50114a3e849d52d3c3c667db6cc3a353f6589239
Ayrat Gazievm <klub724@gmail.com>
2021-03-28 00:15:23 +0500

    section elements animation added

--- script.js
+++ script.js
@@ -2,29 +2,72 @@
 const cluods = document.querySelector(".cluods");
 const rocks = document.querySelector(".rocks");
 const ground = document.querySelector(".ground");
+const sections = document.querySelectorAll("section");
+const menuLink = document.querySelectorAll('a[href*="#"]');
+const animElements = document.querySelectorAll(".anim");
+
+let options = {
+    threshold: 1
+};
+let callback = function (entries, observer) {
+    entries.forEach((entry) => {
+        if (
+            entry.isIntersecting &&
+            entry.target.classList.contains("section__content-num") &&
+            !entry.target.classList.contains("section_num-animated")
+        ) {
+            entry.target.classList.add("section_num-animated");
+        } else if (
+            entry.isIntersecting &&
+            !entry.target.classList.contains("section_animated")
+        ) {
+            entry.target.classList.add("section_animated");
+        }
+    });
+};
+let observer = new IntersectionObserver(callback, options);
+
+animElements.forEach((el) => {
+    observer.observe(el);
+});
 
 document.addEventListener("scroll", (e) => {
     let move = Math.floor(window.scrollY / 5);
     ground.style.transform = `translateY(${move}px)`;
     cluods.style.transform = `translateY(-${move}px)`;
-});
-// Menu
-const menuBtn = document.querySelectorAll(".menu__list-item");
 
-menuBtn.forEach((el) => {
-    el.addEventListener("click", () => {
-        menuBtn.forEach((elem) => elem.classList.remove("selected"));
-        el.classList.add("selected");
+    let winCenter = window.scrollY + window.innerHeight / 2;
+
+    // animElements.forEach((el) => {
+    //     let elCenter =
+    //         el.getBoundingClientRect().top +
+    //         el.getBoundingClientRect().height / 2;
+
+    //     console.log(elCenter);
+
+    //     if (winCenter > elCenter) {
+    //         if (!el.classList.contains("section_animtated")) {
+    //             el.classList.add("section_animtated");
+    //         }
+    //     }
+    // });
+
+    sections.forEach((sec) => {
+        let secCenter = sec.offsetTop + sec.offsetHeight / 2;
+        if (winCenter > secCenter) {
+            menuLink.forEach((el) => el.classList.remove("clicked"));
+            document
+                .querySelector(`a[href*="#${sec.id}"]`)
+                .classList.add("clicked");
+        }
     });
 });
+
 //Smooth scroll
-const menuLink = document.querySelectorAll('a[href*="#"]');
 
 menuLink.forEach((link) => {
     link.addEventListener("click", (e) => {
         e.preventDefault();
-        menuLink.forEach((el) => el.classList.remove("clicked"));
-        link.classList.add("clicked");
         const id = link.getAttribute("href");
         const block = document.querySelector(id);
         block.scrollIntoView({
